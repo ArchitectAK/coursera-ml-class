3fe06a54c2a4cdb1af6037690719b54acfb05bb6
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2019-05-26 14:18:29 +0800

    oneVsAll solved

--- ex3/oneVsAll.m
+++ ex3/oneVsAll.m
@@ -54,6 +54,22 @@ X = [ones(m, 1) X];
 
 
 
+% Some useful variables
+m = size(X, 1);
+n = size(X, 2);
+
+% You need to return the following variables correctly
+all_theta = zeros(num_labels, n + 1);
+
+% Add ones to the X data matrix
+X = [ones(m, 1) X];
+
+initial_theta = zeros(n + 1, 1);
+options = optimset('GradObj', 'on', 'MaxIter', 50);
+for c = 1:num_labels;
+  [theta] = fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), initial_theta, options);
+  all_theta(c,:)=theta'
+endfor
 
 
 
